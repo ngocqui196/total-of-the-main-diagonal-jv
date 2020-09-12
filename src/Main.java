8d6bde9e1216793c3d3d149216acc33814b12533
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2020-09-12 10:36:04 +0700

    total of the main diagonal

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int totalOfTheMainDiagonal(int[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j){
+                    sum += arr[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+       int[][] arr = {
+               {4,3,6,9,5},
+               {7,6,5,4,8},
+               {4,3,9,6,1},
+               {8,4,2,6,5},
+               {7,1,3,9,4}
+       };
+       int result = totalOfTheMainDiagonal(arr);
+        System.out.println(result);
+    }
+
+}
